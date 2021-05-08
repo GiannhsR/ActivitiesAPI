30acb61dbe960e14ca7e24b48b4d4fc8b0002e07
YiannisRak <giannhsrak@hotmail.com>
2021-05-08 17:33:06 +0300

    Repository Pattern

--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,15 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ToDoActivities.DAL;
 using ToDoActivities.DAL.DataContexts;
-using ToDoActivities.DAL.Models;
+using ToDoActivities.DAL.Interfaces;
+using ToDoActivities.DAL.Repositories;
 using ToDoActivities.DAL.RepositoryServices;
+using ToDoActivities.Models;
 
 namespace ToDoActivities
 {
@@ -33,6 +29,7 @@ namespace ToDoActivities
               options.UseSqlServer(Configuration.GetConnectionString("ActivitiesAppDb")));
 
             services.AddScoped<ActivitiesDbService>();
+            services.AddScoped <IGenericRepository<Activity>, GenericRepository<Activity>>();
 
             services.AddCors(options =>
             {
